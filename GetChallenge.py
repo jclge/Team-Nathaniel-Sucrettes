4058b389c5314121e266f76d0888aa02f03aca32
Julien Calenge <julien.calenge@epitech.eu>
2023-03-25 18:31:32 +0100

    challenge getter done

--- GetChallenge.py
+++ GetChallenge.py
@@ -5,9 +5,11 @@ class GetChallenge:
     def __init__(self, challenge: int):
         with open(f"challenge{challenge}.json", 'r') as fd:
             self.__challenge = loads(fd.read())
+            fd.close()
+        self.__private = self.__challenge.pop("final_tests")
 
     def get_public(self) -> dict:
-        self.__challenge
-        return
+        return self.__challenge
 
-g = GetChallenge(0)
+    def get_private(self) -> dict:
+        return self.__private
