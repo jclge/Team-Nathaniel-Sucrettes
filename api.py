b632e118052103d29107981db79eaed4ea72d3ba
MrVeco <alexandre.douard@epitech.eu>
2023-03-29 17:24:03 +0200

    [ADD] link code

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 from copy import copy
+from random import randint
 
 from flask import Flask, jsonify, request
+
+from ExecuteTest import Process
+from GetChallenge import GetChallenge
 
 app = Flask(__name__)
 
@@ -19,8 +23,9 @@ class User:
 TEAMS_NAME = ["no_team", "Blue", "Red", "Pink", "Orange", "Purple", "Green"]
 
 
-class Teams:
-    def __init__(self, id, name, nb_users) -> None:
+class Teams(Process):
+    def __init__(self, id:int, name:str, nb_users:int, challenge_id:int) -> None:
+        super().__init__(challenge_id)
         self.id = id
         self.name = name
         self.nb_users = nb_users
@@ -29,20 +34,30 @@ class Teams:
     def get_users(self):
         return {"name": self.name, "id": self.id, "users": [i.get_user() for i in self.users]}
 
+    # def remove_user(self, id_user:int) -> None:
+    #     self.users.pop()
+
+    def get_nb_player(self) -> int:
+        return len(self.users)
+
 
 class Game:
-    def __init__(self, subject, tests_verif, nb_team) -> None:
+    def __init__(self, nb_team:int, challenge_id:int = randint(0,1)) -> None:
         t_name = copy(TEAMS_NAME)
-        self.teams = [Teams(i, next(t_name, None)) for i in range(nb_team + 1)]
+        self.__challenge_id = challenge_id
+        self.__get_challenge = GetChallenge(self.__challenge_id)
+        self.nb_users = self.__get_challenge.get_public()["tests"]["nb_users"]
+        self.teams = [Teams(i, next(t_name, None), self.nb_users, self.__challenge_id) for i in range(nb_team + 1)]
         self.users = []
-        self.subject = subject
-        self.tests_verif = tests_verif
         self.nb_team = nb_team
 
-    def add_user(self, id, name, id_team) -> bool:
+    def add_user(self, id:int, name:int, id_team:int) -> bool:
         self.users.append(User(id, name, id_team))
 
-    
+    def change_user_team(self, id_user:int, id_team:int):
+        self.teams[self.users[id_user].id_team].remove_user(id_user)
+        self.users[id_user].id_team = id_team
+        self.users[id_user].part = self.teams[id_team].get_nb_player() % self.nb_users
 
 
 games = {}
@@ -56,12 +71,20 @@ def get_exec(id_game, id_user):
 
 
 @app.route('/game/<int:id_game>/<int:id_user>', methods=['GET'])
-def create_game(id_game, id_user, id_team):
+def create_game(id_game, id_user):
     if games[id_game]:
-        games[id_game].add_user(id_user, id_team)
+        games[id_game].add_user(id_user, 0)
     else:
-        games[id_game] = Game()
+        games[id_game] = Game(2)
+        games[id_game].add_user(id_user, 0)
 
+    return jsonify()
+
+@app.route('/game/<int:id_game>/<int:id_user>/<int:id_team>', methods=['GET'])
+def change_team(id_game, id_user, id_team):
+    if not games[id_game] or not games[id_game].teams[id_team] or not games[id_game].users[id_user]:
+        return jsonify(None)
+    games[id_game].change_user_team(id_user, id_team)
     return jsonify()
 
 
