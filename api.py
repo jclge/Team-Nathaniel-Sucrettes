60d457f9ad30a81406e67205240f5a35ab47955b
MrVeco <alexandre.douard@epitech.eu>
2023-04-05 13:35:36 +0200

    [ADD] Ajout de la méthode pour get le id_team avec l'id_user

--- api.py
+++ api.py
@@ -24,21 +24,21 @@ TEAMS_NAME = ["no_team", "Blue", "Red", "Pink", "Orange", "Purple", "Green"]
 
 
 class Teams(Process):
-    def __init__(self, id:int, name:str, nb_users:int, challenge_id:int) -> None:
+    def __init__(self, id: int, name: str, nb_users: int, challenge_id: int) -> None:
         super().__init__(challenge_id)
         self.id = id
         self.name = name
         self.nb_users = nb_users
         self.users = []
 
-    def get_users(self):
+    def get_users(self) -> dict:
         return {"name": self.name, "id": self.id, "users": [i.get_user() for i in self.users]}
 
     def add_user(self, user: User) -> None:
         self.users.append(user)
         self.nb_users += 1
 
-    def remove_user(self, id_user:int) -> None:
+    def remove_user(self, id_user: int) -> None:
         for i in range(self.nb_users):
             if self.users[i].id == id_user:
                 self.users[i].id_team = 0
@@ -46,23 +46,24 @@ class Teams(Process):
                 self.nb_users -= 1
                 break
 
-    def get_nb_player(self) -> int:
+    def get_nb_user(self) -> int:
         return len(self.users)
 
 
 class Game:
-    def __init__(self, nb_team:int, challenge_id:int = randint(0,1)) -> None:
+    def __init__(self, nb_team: int, challenge_id: int = randint(0, 1)) -> None:
         t_name = copy(TEAMS_NAME)
         self.__challenge_id = challenge_id
         self.__get_challenge = GetChallenge(self.__challenge_id)
         self.nb_users = self.__get_challenge.get_public()["tests"]["nb_users"]
-        self.teams = [Teams(i, next(t_name, None), self.nb_users, self.__challenge_id) for i in range(nb_team + 1)]
+        self.teams = [Teams(i, next(t_name, None), 0, self.__challenge_id)
+                      for i in range(nb_team + 1)]
         self.users = []
         self.nb_team = nb_team
 
-    def add_user(self, id:int, name:int, id_team:int) -> bool:
+    def add_user(self, id: int, name: int, id_team: int) -> bool:
         self.users.append(User(id, name, id_team))
-        self.teams[id_team].add
+        # self.teams[id_team].add
 
     def remove_user(self, user_id: int) -> None:
         for i in range(len(self.users)):
@@ -70,10 +71,20 @@ class Game:
                 self.users.pop(i)
                 [team.remove_user(user_id) for team in self.teams]
 
-    def change_user_team(self, id_user:int, id_team:int):
+    def change_user_team(self, id_user: int, id_team: int):
         self.teams[self.users[id_user].id_team].remove_user(id_user)
         self.users[id_user].id_team = id_team
-        self.users[id_user].part = self.teams[id_team].get_nb_player() % self.nb_users
+        self.users[id_user].part = self.teams[id_team].get_nb_player(
+        ) % self.nb_users
+
+    def get_team_with_user(self):
+        # team = [i for i in self.teams if i.get_nb_user() != 0 and ]
+        pass
+
+    def get_team_user_id(self, id_user: int) -> User:
+        for user in self.users:
+            if user.id_user == id_user:
+                return user.id_team
 
 
 games = {}
@@ -88,13 +99,14 @@ def get_exec(id_game, id_user):
 
 @app.route('/game/<int:id_game>/<int:id_user>', methods=['GET'])
 def create_game(id_game, id_user):
-    if games[id_game]:
+    if id_game in games:
         games[id_game].add_user(id_user, 0)
     else:
         games[id_game] = Game(2)
         games[id_game].add_user(id_user, 0)
 
     return jsonify()
+
 
 @app.route('/game/<int:id_game>/<int:id_user>/<int:id_team>', methods=['GET'])
 def change_team(id_game, id_user, id_team):
@@ -103,8 +115,10 @@ def change_team(id_game, id_user, id_team):
     games[id_game].change_user_team(id_user, id_team)
     return jsonify()
 
+
 def delete_game(id_game: int) -> None:
-    del game[id_game]
+    del games[id_game]
+
 
 if __name__ == '__main__':
     app.run(debug=True)
